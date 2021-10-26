4490b2767b863c821038e18d1278bdab08ce5f1f
faktaarief <faktaarief16@gmail.com>
2021-10-26 09:57:24 +0700

    Added Buffered and Range Channel

--- channel_test.go
+++ channel_test.go
@@ -2,6 +2,7 @@ package go_goroutine
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -38,11 +39,13 @@ func TestChannelAsParameter(t *testing.T) {
 	time.Sleep(5 * time.Second)
 }
 
+// Send Only
 func OnlyIn(channel chan<- string) {
 	time.Sleep(2 * time.Second)
 	channel <- "Fakta Arief"
 }
 
+// Receive Only
 func OnlyOut(channel <-chan string) {
 	data := <-channel
 	fmt.Println(data)
@@ -56,4 +59,41 @@ func TestInOutChannel(t *testing.T) {
 	go OnlyOut(channel)
 
 	time.Sleep(5 * time.Second)
+}
+
+func TestBufferedChannel(t *testing.T) {
+	channel := make(chan string, 3)
+	defer close(channel)
+
+	go func() {
+		channel <- "Fakta"
+		channel <- "Arief"
+		channel <- "Farhanto"
+	}()
+
+	go func() {
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+	}()
+
+	time.Sleep(2 * time.Second)
+	fmt.Println("Selesai")
+}
+
+func TestRangeChannel(t *testing.T) {
+	channel := make(chan string)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			channel <- "Perulangan ke" + strconv.Itoa(i)
+		}
+		close(channel)
+	}()
+
+	for data := range channel {
+		fmt.Println("Menerima data", data)
+	}
+
+	fmt.Println("Selesai")
 }
