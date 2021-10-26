2c97ea2583a10b49c5df618c9ad421e7128efe2d
faktaarief <faktaarief16@gmail.com>
2021-10-26 15:42:18 +0700

    Added Race Condition, Mutex, RWMutex, Deadlock, WaitGroup, Once, Pool

---
+++ pool_test.go
@@ -0,0 +1,32 @@
+package go_goroutine
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestPool(t *testing.T) {
+	// pool := sync.Pool{}
+	pool := sync.Pool{
+		New: func() interface{} {
+			return "New"
+		},
+	}
+
+	pool.Put("Fakta")
+	pool.Put("Arief")
+	pool.Put("Farhanto")
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			data := pool.Get()
+			fmt.Println(data)
+			pool.Put(data)
+		}()
+	}
+
+	time.Sleep(3 * time.Second)
+	fmt.Println("Selesai")
+}
